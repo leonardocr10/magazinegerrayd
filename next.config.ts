47c8bf82a1e5a65d0960b2fcd35dac21783d46fe
leonardocr10 <leonardocr_10@hotmail.com>
2026-08-03 16:50:19 -0300

    Restore default export output directory

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: ".next-static",
   images: {
     unoptimized: true,
     remotePatterns: [
